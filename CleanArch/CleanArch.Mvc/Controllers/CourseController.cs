76f4ae0d7b417667cf25fc17a3ad6353987c6f86
KingSalami8005 <mufasa8005@gmail.com>
2020-07-17 15:56:26 +0200

    Commit changes for Presentation Course Controller, Views, Styling, Authentication.

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _service;
+
+        public CourseController(ICourseService service)
+        {
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _service.GetCourses();
+
+            return View(model);
+        }
+    }
+}
